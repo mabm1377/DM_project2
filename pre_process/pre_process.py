153e383fe120ca64a02cba21534a85ed2e7188ed
ali <mabm13770@gmail.com>
2020-06-06 14:59:42 +0430

    first question done

--- pre_process/pre_process.py
+++ pre_process/pre_process.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 import os
 
 names = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status', 'post',
          'relationship', 'nation', 'gender', 'capital_gain', 'capital_loss', 'hours_per_week',
          'country',
          'wealth']
-
+names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers = list(set(names) - {'wealth'})
 converted_quantitative_data_to_qualitative_dict = {}
 
 
@@ -20,33 +22,21 @@ def read_file():
     return df
 
 
-def remove_missing_value(data):
-    data = data.replace(' ?', np.NaN)
-    data.dropna(inplace=True)
-    return data
+def remove_missing_value(df):
+    df = df.replace(' ?', np.NaN)
+    df.dropna(inplace=True)
+    df.to_csv("deletedMissingValue.csv")
+    return df
 
 
-def remove_outliers_by_age(data):
-    column = data['age']
-    quantile1, quantile3 = np.percentile(column, [25, 75])
-    iqr = quantile3 - quantile1
-    lower_bound_val = quantile1
-    upper_bound_val = quantile3
-    for i in range(len(data)):
-        # a = column[i]
-        if i in data.index:
-            if column[i] < lower_bound_val or column[i] > upper_bound_val:
-                data = data.drop(i)
-    return data
+def draw_box_plot(df):
+    for name in names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers:
+        plt.boxplot(df[name])
+        plt.savefig(name + ".png")
 
 
-def remove_outliers_by_work_class(data):
-    pass
-
-
-#
 def convert_quantitative_data_to_qualitative(data):
-    for name in names:
+    for name in names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers:
         column = data[name]
         if column.dtype != "int64":
             distinct_elements = column.drop_duplicates()
@@ -54,33 +44,53 @@ def convert_quantitative_data_to_qualitative(data):
             for i, elem in enumerate(distinct_elements):
                 b = separation_length * (i + 1)
                 data = data.replace(str(elem), b)
-                converted_quantitative_data_to_qualitative_dict[str(elem)] = b
+                if name not in converted_quantitative_data_to_qualitative_dict:
+                    converted_quantitative_data_to_qualitative_dict[name] = {b: str(elem)}
+                else:
+                    converted_quantitative_data_to_qualitative_dict[name].update({b: str(elem)})
+
     return data
+
+
+def remove_outliers_by_column(df, name):
+    column = df[name]
+    quantile1, quantile3 = np.percentile(column, [25, 75])
+    iqr = quantile3 - quantile1
+    lower_bound_val = quantile1
+    upper_bound_val = quantile3
+    for i in range(len(df)):
+        # a = column[i]
+        if i in df.index:
+            if column[i] < lower_bound_val or column[i] > upper_bound_val:
+                df = df.drop(i)
+    return df
+
+
+def remove_outliers(df):
+    for name in names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers:
+        print(name)
+        df = remove_outliers_by_column(df, name)
+        print(remove_outliers_by_column(df, name).head())
+        print("___________________________________________")
+        print(len(df))
+        print("*******************************************")
+    df.to_csv("removed_outliers_data.csv")
+    return df
 
 
 print("____________________read_file_______________________________")
 df = read_file()
 print(len(df))
-print("_____________________________________________________________")
 
 print("____________________remove_missing_value_______________________________")
 df = remove_missing_value(read_file())
 print(len(df))
-print("_____________________________________________________________")
-print(df.head())
-print("_____________________________________________________________")
-print(convert_quantitative_data_to_qualitative(df).head())
-print(converted_quantitative_data_to_qualitative_dict)
-# print(df.head())
 
-#
-# print(df.head())
-#
-# print("____________________remove_outliers_by_one_column_______________________________")
-# df = remove_outliers_by_age(df)
-# print(len(df))
+print("____________________convert_quantitative_data_to_qualitative_______________________________")
+df = convert_quantitative_data_to_qualitative(df)
 
-# print(remove_outliers_by_on_column(df, 'age')[1])
-# for key, value in df.iteritems():
-#     print(key)
-#     print(value)
+print("____________________draw_box_plot______________________________")
+draw_box_plot(df)
+
+print("____________________remove_outliers______________________________")
+remove_outliers(df)
