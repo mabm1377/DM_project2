e27f8198184af4b3f49113542f5faa2f2c777e83
ali <mabm13770@gmail.com>
2020-06-07 10:34:40 +0430

    pre_process.py ended and tested

--- pre_process/pre_process.py
+++ pre_process/pre_process.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 import os
 
 names = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status', 'post',
          'relationship', 'nation', 'gender', 'capital_gain', 'capital_loss', 'hours_per_week',
          'country',
          'wealth']
-names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers = list(set(names) - {'wealth'})
-converted_quantitative_data_to_qualitative_dict = {}
+names_for_converting_qualitative_data_to_quantitative_dict_ad_outliers = list(set(names) - {'wealth'})
+converted_qualitative_data_to_quantitative_dict = {}
 
 
 def read_file():
@@ -30,13 +30,13 @@ def remove_missing_value(df):
 
 
 def draw_box_plot(df):
-    for name in names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers:
-        plt.boxplot(df[name])
+    for name in names_for_converting_qualitative_data_to_quantitative_dict_ad_outliers:
+        sns.boxplot(data=df, x=df[name])
         plt.savefig(name + ".png")
 
 
-def convert_quantitative_data_to_qualitative(data):
-    for name in names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers:
+def convert_qualitative_data_to_quantitative(data):
+    for name in names_for_converting_qualitative_data_to_quantitative_dict_ad_outliers:
         column = data[name]
         if column.dtype != "int64":
             distinct_elements = column.drop_duplicates()
@@ -44,22 +44,27 @@ def convert_quantitative_data_to_qualitative(data):
             for i, elem in enumerate(distinct_elements):
                 b = separation_length * (i + 1)
                 data = data.replace(str(elem), b)
-                if name not in converted_quantitative_data_to_qualitative_dict:
-                    converted_quantitative_data_to_qualitative_dict[name] = {b: str(elem)}
+                if name not in converted_qualitative_data_to_quantitative_dict:
+                    converted_qualitative_data_to_quantitative_dict[name] = {b: elem}
                 else:
-                    converted_quantitative_data_to_qualitative_dict[name].update({b: str(elem)})
+                    converted_qualitative_data_to_quantitative_dict[name].update({b: elem})
 
     return data
+
+
+def back_quantitative_data(df, path):
+    for name in converted_qualitative_data_to_quantitative_dict:
+        k = converted_qualitative_data_to_quantitative_dict[name]
+        df[name] = df[name].map(k)
+    df.to_csv(path)
 
 
 def remove_outliers_by_column(df, name):
     column = df[name]
     quantile1, quantile3 = np.percentile(column, [25, 75])
-    iqr = quantile3 - quantile1
     lower_bound_val = quantile1
     upper_bound_val = quantile3
     for i in range(len(df)):
-        # a = column[i]
         if i in df.index:
             if column[i] < lower_bound_val or column[i] > upper_bound_val:
                 df = df.drop(i)
@@ -67,30 +72,28 @@ def remove_outliers_by_column(df, name):
 
 
 def remove_outliers(df):
-    for name in names_for_converted_quantitative_data_to_qualitative_dict_ad_outliers:
+    for name in names_for_converting_qualitative_data_to_quantitative_dict_ad_outliers:
         print(name)
         df = remove_outliers_by_column(df, name)
-        print(remove_outliers_by_column(df, name).head())
-        print("___________________________________________")
-        print(len(df))
         print("*******************************************")
-    df.to_csv("removed_outliers_data.csv")
+    df.to_csv("removed_outliers_quantitative_data.csv")
     return df
 
 
 print("____________________read_file_______________________________")
 df = read_file()
-print(len(df))
 
 print("____________________remove_missing_value_______________________________")
 df = remove_missing_value(read_file())
-print(len(df))
 
 print("____________________convert_quantitative_data_to_qualitative_______________________________")
-df = convert_quantitative_data_to_qualitative(df)
+df = convert_qualitative_data_to_quantitative(df)
 
 print("____________________draw_box_plot______________________________")
 draw_box_plot(df)
 
+
 print("____________________remove_outliers______________________________")
 remove_outliers(df)
+
+back_quantitative_data(df.copy(), path="removed_outliers_qualitative_data.csv")
